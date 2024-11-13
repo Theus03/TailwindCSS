dcefc5a8717247ef179bfcddd12dbc2b5949f826
Matheus Leite <matheusssleite@outlook.com>
2024-11-13 18:35:44 -0300

    Fazendo um teste com as principais classes do tailwindcss

---
+++ src/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "media",
+  theme: {
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      '2xl': ['1.5rem', {
+        lineHeight: '2rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      '3xl': ['1.875rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+    },
+    screens: {
+      'sm': '640px', // => @media (min-width: 640px) { ... }
+      'md': '768px', // => @media (min-width: 768px) { ... }
+      'lg': '1024px', // => @media (min-width: 1024px) { ... }
+      'xl': '1280px', // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px', // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
